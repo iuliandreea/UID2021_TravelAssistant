59f559255574d01514c49b55635a2f481020bc73
Ana-Maria Morosan <anamaria_ana_30@yahoo.com>
2022-01-09 15:56:45 +0200

    add visit fragment

---
+++ app/src/main/java/com/example/travelassistant/AttractionRecyclerAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.travelassistant
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AttractionRecyclerAdapter(val context: Context?) :
+    RecyclerView.Adapter<AttractionRecyclerAdapter.MyViewHolder>() {
+    var attractionList: List<Attraction> = listOf()
+
+    class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val id: TextView = itemView!!.findViewById(R.id.id)
+        val name: TextView = itemView!!.findViewById(R.id.name)
+        val address: TextView = itemView!!.findViewById(R.id.address)
+        val price: TextView = itemView!!.findViewById(R.id.entryPrice)
+        val contact: TextView = itemView!!.findViewById(R.id.contact)
+        val description: TextView = itemView!!.findViewById(R.id.description)
+        val image: ImageView = itemView!!.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.attraction_content, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.id.text = attractionList.get(position).id
+        holder.name.text = attractionList.get(position).name
+        holder.address.text = attractionList.get(position).address
+        holder.price.text = attractionList.get(position).price + " EUR"
+        holder.contact.text = attractionList.get(position).contact
+        holder.description.text = attractionList.get(position).description
+        holder.image.setImageResource(attractionList.get(position).image)
+    }
+
+    override fun getItemCount(): Int {
+        return attractionList.size
+    }
+
+    fun setAttractionListItems(list: List<Attraction>) {
+        this.attractionList = list;
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/travelassistant/ExploreFragment.kt
+++ app/src/main/java/com/example/travelassistant/ExploreFragment.kt
@@ -1,10 +1,17 @@
 package com.example.travelassistant
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import java.text.SimpleDateFormat
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,7 +43,79 @@ class ExploreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val views = inflater.inflate(R.layout.fragment_explore, container, false)
+        val visitBtn = views.findViewById<Button>(R.id.button4)
+        visitBtn.setOnClickListener {
+            if (validateInput(views)) {
+                views.findNavController().navigate(R.id.visitFragment)
+            }
+        }
+
         return views
+    }
+
+
+    fun displayErrorMessage(message: String) {
+        val dialogClickListener =
+            DialogInterface.OnClickListener { dialog, which ->
+                when (which) {
+                    DialogInterface.BUTTON_POSITIVE -> {
+                    }
+                    DialogInterface.BUTTON_NEGATIVE -> {
+
+                    }
+                }
+            }
+        println(message)
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        builder.setMessage(message)
+            .setPositiveButton("OK", dialogClickListener)
+            .show()
+    }
+
+    fun validateInput(views: View): Boolean {
+        val dateStr = views.findViewById<EditText>(R.id.checkIn)
+        val dateStr1 = views.findViewById<EditText>(R.id.checkOut)
+        val destination = views.findViewById<EditText>(R.id.editTextTextPersonName2)
+        val dest = destination.text.toString()
+        if (dest.equals("Destination") || dest.trim().equals("")) {
+
+            displayErrorMessage("Invalid Destination!")
+
+        } else {
+
+            var formatter = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+            try {
+
+                val date = formatter.parse(dateStr.text.toString())
+                if (dateStr.text.toString().length != 10) {
+
+                    displayErrorMessage("Invalid date in")
+
+                } else {
+                    println(date.toString())
+
+                    if (dateStr1.text.toString().length != 10) {
+
+                        displayErrorMessage("Invalid date out")
+
+                    } else {
+                        try {
+                            val date1 = formatter.parse(dateStr1.text.toString())
+                            println(date1.toString())
+                            return true
+                        } catch (e: Exception) {
+                            displayErrorMessage("invalid date out")
+
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                displayErrorMessage("invalid date in")
+
+            }
+
+        }
+        return false
     }
 
 
